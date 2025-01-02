4a8c8cbf2060b80090ae35b1dfbeadc6ffb590ab
Alberes Silva <alberessilva@yahoo.com.br>
2025-01-02 20:25:23 -0300

    Create type.py

---
+++ type.py
@@ -0,0 +1,15 @@
+print("Python")
+print(type("Meu nome é Juliano!"))
+print(type(500))
+print(type("500"))
+print(type(50.0))
+print("olá" + " Juliano")
+print(5 + 3)
+print(" Python" * 3)
+
+#Contagem
+print(len("Juliano"))
+print(len("          "))
+
+#Como saber o que o comando faz
+help(print)
