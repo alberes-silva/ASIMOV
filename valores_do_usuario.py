0f0a21929db82e72e70ad006e436cb8d21b9b039
Alberes Silva <alberessilva@yahoo.com.br>
2025-01-02 21:16:15 -0300

    Create valores_do_usuario.py

---
+++ valores_do_usuario.py
@@ -0,0 +1,15 @@
+nome = input("Digite o seu nome: ")
+print("Olá " + nome)
+
+numero = input("Digite um número: ")
+print("O número digitado é " + numero)
+
+num = input("Digite um número: ")
+x_num = int(num)
+print(10 + x_num)
+
+num2 = int(input("Digite um número: "))
+print("O número digitado é ", num2)
+
+num3 = int(input("Digite um número: "))
+print(15 + num3)
