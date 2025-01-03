5b0d6f65082fe1678434c876eab2fca86a07ed2f
Alberes Silva <alberessilva@yahoo.com.br>
2025-01-02 21:04:17 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,31 @@
+print(3.14 * 5 ** 2)
+
+pi = 3.14
+raio = 5
+raio_ao_quadrado = raio ** 2
+
+
+print(pi * raio_ao_quadrado)
+
+#como definir os nomes das variavéis_finalizing
+
+x = 4
+print(x)
+
+#Errado
+#meu nome = "Juliano"
+#print(meu nome)
+
+meu_nome = "Juliano"
+print(meu_nome)
+
+x = 5
+# Errado - print(X)
+
+a1 = 6
+print(a1)
+
+#Errado
+#1a = "Juliano"
+#print(1a)
+
